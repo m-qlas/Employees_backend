83a6b6ab8d3d16706dacc0c6fdefd9133981d954
Mateusz Kulas <mateusz@debianMK.debianMK>
2020-05-27 15:46:25 +0200

    Little improvements

--- src/main/java/com/qlas/controller/ViewController.java
+++ src/main/java/com/qlas/controller/ViewController.java
@@ -2,13 +2,21 @@ package com.qlas.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ViewController {
 
 	@RequestMapping("/")
 	public String view() {
-		System.out.println("Function started");
-		return "index.html";
+		System.out.println("Default function started");
+		return "home.html";
+	}
+	
+	@RequestMapping("/login")
+	public String view(@RequestParam("login") String login, @RequestParam("password") String pass) {
+		System.out.println("Parameterd function started");
+		System.out.println("Login: " + login + " Password: " + pass);
+		return "home.html";
 	}
 }
--- src/main/webapp/indexScript.js
+++ src/main/webapp/indexScript.js
@@ -1,1 +1,4 @@
 let btn1 = document.querySelector("button");
+btn1.addEventListener("click", ()=>{
+	console.log("Enter without login");
+});
