9b5e28e6bf67fa8c20ff0213a9ed0cd361317307
Mateusz Kulas <mateusz@debianMK.debianMK>
2020-07-02 04:44:59 +0200

    Finished hardcoded basic authorization

--- src/main/java/com/qlas/controller/EmployeeContoller.java
+++ src/main/java/com/qlas/controller/EmployeeContoller.java
@@ -3,6 +3,7 @@ package com.qlas.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.qlas.repository.LaptopRepo;
 import com.qlas.repository.SalaryRepo;
 
 @RestController
+@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
 public class EmployeeContoller {
 
 	@Autowired
--- src/main/java/com/qlas/security/SecurityConfig.java
+++ src/main/java/com/qlas/security/SecurityConfig.java
@@ -25,78 +25,68 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
-	@Autowired
-	private UserDetailsService userDetailsService;
+//	@Autowired
+//	private UserDetailsService userDetailsService;
+//	
+//	
+//	@Bean
+//	public AuthenticationProvider authProvider() {
+//		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+//		provider.setUserDetailsService(userDetailsService);
+//		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+//		//provider.setPasswordEncoder(new BCryptPasswordEncoder());
+//		return provider;
+//	}
 	
-	@Autowired
-	private RestAuthEntryPoint restAuthEntryPoint;
-
-	@Bean
-	public AuthenticationProvider authProvider() {
-		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-		provider.setUserDetailsService(userDetailsService);
-		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
-		//provider.setPasswordEncoder(new BCryptPasswordEncoder());
-		return provider;
-	}
-	
-	@Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS"));
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-
-        return  source;
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        .csrf().disable()   
+        .authorizeRequests()
+        .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            //.formLogin().and()
+            .httpBasic();
     }
 	
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-		.cors(withDefaults())
-		.and()
-				.exceptionHandling()
-				.authenticationEntryPoint(restAuthEntryPoint)
-			.and()
-				.authorizeRequests()
-				.antMatchers(HttpMethod.GET,"/user").permitAll()
-				.anyRequest().authenticated()
-			.and()
-				.formLogin()
-				.loginProcessingUrl("/performLogin")
-			
-			.and()
-				.csrf().disable();
-		
-		
-		//.and().formLogin();
-//		.loginPage("/login").permitAll();
-//			.defaultSuccessUrl("/get",true)
-//			.and()
-//			.logout().invalidateHttpSession(true)
-//			.clearAuthentication(true)
-//			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-//			.logoutSuccessUrl("/logout-succes").permitAll().deleteCookies("JSESSIONID");
-	}
-
 //	@Bean
-//	public RequestCache refererRequestCache() {
-//		return new HttpSessionRequestCache() {
-//			@Override
-//			public void saveRequest(HttpServletRequest request, HttpServletResponse response) {
-//				String referrer = request.getHeader("referer");
-//				if (referrer != null) {
-//					request.getSession().setAttribute("SPRING_SECURITY_SAVED_REQUEST",
-//							new SimpleSavedRequest(referrer));
-//				}
-//			}
-//		};
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//
+//        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+//        configuration.setAllowedHeaders(List.of("*"));
+//        configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS"));
+//        configuration.setAllowCredentials(true);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//
+//        return  source;
+//    }
+	
+
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http
+//		.cors()
+//		.and()
+//				.exceptionHandling()
+//				.authenticationEntryPoint(restAuthEntryPoint)
+//			.and()
+//				.authorizeRequests()
+//				.antMatchers(HttpMethod.GET,"/user").permitAll()
+//				.anyRequest().authenticated()
+//			.and()
+//				.formLogin()
+//				.loginPage("/login")
+//				.loginProcessingUrl("/performLogin")
+//			
+//			.and()
+//				.csrf().disable();
 //	}
+	
+	
+
 
 }
